04ad97feb54f3a78cb91e9b3d99a908313a798b7
Joshua Coetzer <97086935+JoshuaCoetzer-glucode@users.noreply.github.com>
2022-09-07 23:46:52 +0200

    Added remaining unit tests
    Added remaining unit tests and removed UI unit tests as I've never worked with them and am unsure of how they work.

--- JoshAssessment/JoshuaAssessment/JoshuaAssessmentTests/EventsTests.swift
+++ JoshAssessment/JoshuaAssessment/JoshuaAssessmentTests/EventsTests.swift
@@ -17,6 +17,49 @@ class EventsTests: XCTestCase {
             XCTAssertEqual(16, self.systemUnderTest.events?.count)
         }
     }
+    
+    func testTitle() throws {
+        group.notify(queue: .main) {
+            XCTAssertEqual("Liverpool v Porto", self.systemUnderTest.events?[0].title)
+        }
+    }
+    
+    func testSubTitle() throws {
+        group.notify(queue: .main) {
+            XCTAssertEqual("UEFA Champions League", self.systemUnderTest.events?[0].subtitle)
+        }
+    }
+    
+    func testDate() throws {
+        group.notify(queue: .main) {
+            XCTAssertEqual("2022-09-07T01:19:59.687Z", self.systemUnderTest.events?[0].date)
+        }
+    }
+    
+    func testImageUrl() throws {
+        group.notify(queue: .main) {
+            XCTAssertEqual("https://firebasestorage.googleapis.com/v0/b/dazn-recruitment/o/310176837169_image-header_pDach_1554579780000.jpeg?alt=media&token=1777d26b-d051-4b5f-87a8-7633d3d6dd20", self.systemUnderTest.events?[0].image)
+        }
+    }
+    
+    func testVideoUrl() throws {
+        group.notify(queue: .main) {
+            XCTAssertEqual("https://firebasestorage.googleapis.com/v0/b/dazn-recruitment/o/310176837169_image-header_pDach_1554579780000.jpeg?alt=media&token=1777d26b-d051-4b5f-87a8-7633d3d6dd20", self.systemUnderTest.events?[0].video)
+        }
+    }
+    
+    func testAscendingOrder() throws {
+        group.notify(queue: .main) {
+            var inAscending = true
+            for i in 0...(self.systemUnderTest.events?.count ?? 0) {
+                if Date.getFromString(dateString: self.systemUnderTest.events?[i].date ?? "") >= Date.getFromString(dateString: self.systemUnderTest.events?[i + 1].date ?? "") {
+                    inAscending = false
+                    break
+                }
+            }
+            XCTAssertTrue(inAscending)
+        }
+    }
 
 }
 
--- JoshAssessment/JoshuaAssessment/JoshuaAssessmentTests/SchedulesTests.swift
+++ JoshAssessment/JoshuaAssessment/JoshuaAssessmentTests/SchedulesTests.swift
@@ -17,6 +17,43 @@ class SchedulesTests: XCTestCase {
             XCTAssertEqual(16, self.systemUnderTest.schedules?.count)
         }
     }
+    
+    func testTitle() throws {
+        group.notify(queue: .main) {
+            XCTAssertEqual("Liverpool v Porto", self.systemUnderTest.schedules?[0].title)
+        }
+    }
+    
+    func testSubTitle() throws {
+        group.notify(queue: .main) {
+            XCTAssertEqual("UEFA Champions League", self.systemUnderTest.schedules?[0].subtitle)
+        }
+    }
+    
+    func testDate() throws {
+        group.notify(queue: .main) {
+            XCTAssertEqual("2022-09-07T01:19:59.687Z", self.systemUnderTest.schedules?[0].date)
+        }
+    }
+    
+    func testImageUrl() throws {
+        group.notify(queue: .main) {
+            XCTAssertEqual("https://firebasestorage.googleapis.com/v0/b/dazn-recruitment/o/310176837169_image-header_pDach_1554579780000.jpeg?alt=media&token=1777d26b-d051-4b5f-87a8-7633d3d6dd20", self.systemUnderTest.schedules?[0].image)
+        }
+    }
+    
+    func testAscendingOrder() throws {
+        group.notify(queue: .main) {
+            var inAscending = true
+            for i in 0...(self.systemUnderTest.schedules?.count ?? 0) {
+                if Date.getFromString(dateString: self.systemUnderTest.schedules?[i].date ?? "") >= Date.getFromString(dateString: self.systemUnderTest.schedules?[i + 1].date ?? "") {
+                    inAscending = false
+                    break
+                }
+            }
+            XCTAssertTrue(inAscending)
+        }
+    }
 
 }
 
